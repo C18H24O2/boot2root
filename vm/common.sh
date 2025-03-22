ce11b70d21d058a2df3443010e9100a139202c7c
xtrm <oss@xtrm.me>
2025-03-22 20:00:37 +0100

    🔨 chore: better vm scripts
    Signed-off-by: xtrm <oss@xtrm.me>

---
+++ vm/common.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env false
+
+set -euo pipefail
+
+if [ -z "$HOME" ]; then
+	echo "ERROR: HOME is not set"
+	exit 1
+fi
+
+if [ -d "$HOME/sgoinfre" ]; then
+	ISO_DIR="$HOME/sgoinfre/isos"
+else
+	ISO_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/42/isos
+fi
+mkdir -vp "$ISO_DIR"
+
+ISO="$ISO_DIR/rainfall.iso"
+ISO_URL="https://cdn.intra.42.fr/isos/BornToSecHackMe-v1.1.iso"
+
+if [ ! -f "$ISO" ]; then
+	echo "Downloading $ISO"
+	curl -L -o "$ISO" "$ISO_URL"
+fi
+
+echo "To get the machine's IP address, run one of the following:"
+echo " ip neigh show"
+echo " arp -a"
+echo "If those don't work, tough luck."
--- vm/run.sh
+++ vm/run.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-ISO="boot2root.iso"
-
-if [ ! -f "$ISO" ]; then
-    echo "Downloading $ISO"
-    curl -L -o "$ISO" "https://cdn.intra.42.fr/isos/BornToSecHackMe-v1.1.iso"
-fi
+. $(dirname $0)/common.sh
 
 echo "Running VM"
-VM_NAME="boot2root_VM"
+VM_NAME="SnowCrash_VM"
 VBoxManage unregistervm "$VM_NAME" --delete || true
 VBoxManage createvm --name "$VM_NAME" --register
 VBoxManage modifyvm "$VM_NAME" --memory 2048 --cpus 2 --ostype Debian --nic1 nat
@@ -18,9 +11,3 @@ VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide
 VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$ISO"
 VBoxManage modifyvm "$VM_NAME" --nic1 bridged --bridgeadapter1 "${1:-eno2}"
 VBoxManage startvm "$VM_NAME"
-
-echo "To get the machine's IP address, run one of the following:"
-echo " ip neigh show"
-echo " arp -a"
-echo "If those don't work, tough luck."
-
