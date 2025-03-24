bccb93b6dc40dbbc0e241d3fed45ebb276c978c4
xtrm <oss@xtrm.me>
2025-03-24 21:26:01 +0100

    🔨 chore: change nic to hostonly
    Signed-off-by: xtrm <oss@xtrm.me>

--- shell.nix
+++ shell.nix
@@ -4,9 +4,15 @@
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    net-snmp
+    sshpass
+    feroxbuster
     wireshark
     nmap
     john
     qemu
-  ];
+    python312
+  ] ++ (with pkgs.python312Packages; [
+      paramiko
+    ]);
 }
